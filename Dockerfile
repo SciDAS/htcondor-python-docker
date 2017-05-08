ce5f70176cc59d548da2e80149e409041ee1a03e
Alan Hinchliff <hincha@renci.org>
2017-05-08 13:54:41 -0400

    not critical. needed condor_pool user in cluster

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,11 @@ COPY resources/libcondor_utils_8_6_2.so /usr/local/lib/
 #COPY resources/libboost_python.so.1.63.0 /usr/local/lib/
 COPY resources/libclassad.so.8.6.2 /usr/local/lib/
 COPY resources/libcrypto.so.1.0.1e /usr/local/lib/
+COPY resources/libpcre.so.1.2.0 /usr/local/lib/
 COPY condor_config /etc/condor/
 
 RUN cd /usr/local/lib && \
     ln -s libclassad.so.8.6.2 libclassad.so.8 && \
     ln -s libcrypto.so.1.0.1e libcrypto.so.10 && \
-    ln -s /lib/x86_64-linux-gnu/libpcre.so.3.13.1 libpcre.so.1
+    ln -s libpcre.so.1.2.0 libpcre.so.1
 
--- examples/htcondor_test.py
+++ examples/htcondor_test.py
@@ -4,9 +4,9 @@ import os
 
 # need to have a user that is also in the HTCondor cluster
 #os.system("useradd -m -s /bin/bash dmr")
-os.system("groupadd -g 996 condor")
-os.system("useradd -m -s /bin/bash -u 998 -g 996 condor")
-os.system("useradd -U -m -s /bin/bash dmr")
+#os.system("groupadd -g 996 condor")
+#os.system("useradd -m -s /bin/bash -u 998 -g 996 condor")
+#os.system("useradd -U -m -s /bin/bash dmr")
 #os.system("cat /etc/passwd")
 
 # check configuration
@@ -20,9 +20,9 @@ scheddAd = coll.locate(htcondor.DaemonTypes.Schedd, "condor-submitter")
 schedd = htcondor.Schedd(scheddAd)
 
 # submit a test job
-#print str(schedd.submit({"Cmd": "/bin/echo"}))
+print str(schedd.submit({"Cmd": "/bin/echo"}))
 #print str(schedd.submit(classad.ClassAd( { "Cmd": "/bin/echo", "NumCkpts": "0", "MinHosts": "1", "JobNotification": "0", "LastSuspensionTime": "0" })))
 
 ad = classad.parseOne(open("/examples/echo_test.submit.ad"))
-print schedd.submit(ad)
+#print schedd.submit(ad)
 
